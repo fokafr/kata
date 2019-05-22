bfd5d039c6c225daef511a9b88e3a76e8411dc37
fokafr@yahoo.fr <fokafr@yahoo.fr>
2019-05-22 17:47:33 +0200

    map and flatMap

--- src/main/java/com/training/stream/Exo2.java
+++ src/main/java/com/training/stream/Exo2.java
@@ -1,7 +1,10 @@
 package com.training.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*L'API Stream est contenue dans le package java.util.stream. 
 Ce package est composée de 10 interfaces, deux classes et une énumération*/
@@ -22,6 +25,24 @@ public class Exo2 {
 		resultat = personnes.stream().filter(p-> p.getGenre() == Genre.FEMME).mapToInt(p -> p.getTaille()).average().getAsDouble();	
 		
 		System.out.println(resultat);
+		
+		//Map et FlatMap
+		//La méthode map() renvoie un seul élément
+		//La méthode flatMap() renvoie un Stream qui peut contenir zéro, un ou plusieurs éléments
+		
+		List<Integer> nombres = Arrays.asList(1,3,5,7,9);
+		List<List<Integer>> tuples = nombres.stream().map(nombre -> Arrays.asList(nombre-1,nombre))
+											.collect(Collectors.toList());
+		System.out.println("Avec les map "+tuples);		
+		
+		//****************************************************************************************
+		List<Integer> nbrs = Arrays.asList(1,3,5,7,9);
+		//L'opération flatMap() attend en paramètre une Function qui renvoie un Stream
+		List<Integer> nombresDesTuples = nbrs.stream().flatMap(nombre -> Arrays.asList(nombre-1,nombre).stream())
+											.collect(Collectors.toList());
+		System.out.println("Avec les flatMap "+nombresDesTuples);	
+		
+		
 	}
 
 }
