3f558d8909dc0bcd528b5e46a1acaa90fbca4cfc
fokafr@yahoo.fr <fokafr@yahoo.fr>
2019-06-20 17:41:28 +0200

    Algoritmique Dichotomie BinarySearch

--- src/main/java/com/training/other/AlgoNumber.java
+++ src/main/java/com/training/other/AlgoNumber.java
@@ -2,6 +2,8 @@ package com.training.other;
 
 import java.util.Arrays;
 
+
+
 /*
  * • O(log n) Ce sont des algorithmes tr`es rapides. Exemples typiques : recherche dichotomique,
 	exponentiation rapide, etc.
@@ -23,17 +25,77 @@ deux matrices, transpos´ee d’une matrice, tri insertion, tri bulle, tri selec
 }*/
 public class AlgoNumber {
 
-	public static int distClosestNumbers(int[] tab) {
+
+public static int distClosestNumbers(int[] tab) {
 		Arrays.sort(tab);
 		int result = 0;
-		int inf =0;
-		int sup = 0;
+		int a = 0;
+		int b = 0;
 		
 		for(int i=0 ;i < tab.length-2; i++) {
-			inf = tab[i+1] - tab[i];
-			
+			a = tab[i+1] - tab[i];
+			b = tab[i+2] - tab[i+1];
+			if (a <=  b) result = a;
+			else result = b;
 		}
-		return 0;
+		return result;
 	}
+
+/*/
+ * double MonteCarlo(int n) {
+  int k=0;
+  for (int i=0; i<n; ++i) {
+    uint64_t x=rand(), y=rand();
+    if (x*x + y*y <= RR) k++;
+  }
+  return 4.0*k/n;
+}
+ */
+
+public static double calculPi(int nbreLanceTotal) {
+	
+	double x = 0.0;
+	double y = 0.0;
+	int nbreLanceReussi = 0;
+	for(int i=0; i <= nbreLanceTotal; i++ ) {
+		 x = Math.random();
+		 y = Math.random();
+		
+		if((Math.pow(x, 2)+Math.pow(y, 2)) < 1) {
+			nbreLanceReussi++;
+		}
+		
+	}
+
+	
+	return 4 * nbreLanceReussi/Double.valueOf(nbreLanceTotal);	
+}
+
+// la Dichotomie
+
+static float fonctionTest ( float y)
+{
+  return 6*y*y -7*y +1 ;
+}
+
+public static double dich(float inf, float sup, float precision) {
+	float milieu;
+	milieu = (inf+sup)/2;
+	
+	while(Math.abs(sup-inf)> precision) {
+		if(AlgoNumber.fonctionTest(milieu) < 0) sup = milieu;
+		else inf = milieu;
+		milieu = (inf+sup)/2;
+	}
+	
+	return sup;
+}
+
+
+
+public static void main(String[] args) {
+	System.out.println(AlgoNumber.calculPi(10000));
+	
+}
 	
 }
--- src/test/java/com/kata/training/kata/BiscuitTest.java
+++ src/test/java/com/kata/training/kata/BiscuitTest.java
@@ -9,10 +9,12 @@ import org.hamcrest.collection.IsCollectionWithSize;
 import org.hamcrest.core.AnyOf;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsCollectionContaining;
+import org.hamcrest.core.IsEqual;
 import org.mockito.internal.matchers.Contains;
 
 import com.training.beans.Biscuit;
 import com.training.beans.Person;
+import com.training.other.AlgoNumber;
 
 import junit.framework.TestCase;
 
@@ -28,6 +30,9 @@ public class BiscuitTest extends TestCase{
 	      List<Person> collection = Arrays.asList(sara, eva, viktor, anna);
 	      assertThat(collection, IsCollectionWithSize.hasSize(4));
 	      assertThat(collection, IsCollectionContaining.hasItem(eva));
-
+          
+	      //test distClosestNumbers de la classe AlgoNumber
+	      int[] testArray = {3, 9, 50, 15, 99, 7, 96, 65};
+	      assertThat(AlgoNumber.distClosestNumbers(testArray), Is.is(3));
 	    }
 }
